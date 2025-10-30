c1a38186c40b710cbe5b0df12a17d26070014387
Pablo Hernández <phernandezm07@gmail.com>
2025-10-30 16:07:46 -0500

    fix(formatting): correct option name from formatter_by_ft to formatters_by_ft

--- after/plugin/general.lua
+++ after/plugin/general.lua
@@ -5,176 +5,174 @@ local plugin_cmds = vim.api.nvim_create_augroup("PluginEvents", { clear = true }
 vim.o.termguicolors = true
 
 require("kanagawa").setup({
-  theme = "kanagawa-dragon",
-  undercurl = true,
-  overrides = function(colors)
-    return {
-      SpellBad = {
-        sp = colors.palette.katanaGray
-      }
-    }
-  end,
+	theme = "kanagawa-dragon",
+	undercurl = true,
+	overrides = function(colors)
+		return {
+			SpellBad = {
+				sp = colors.palette.katanaGray,
+			},
+		}
+	end,
 })
 
 vim.cmd("colorscheme kanagawa-dragon")
 
 -- Formatting
 require("conform").setup({
-  formatter_by_ft = {
-    lua = { "stylua" },
-    nix = { "alejandra" },
-  },
-  format_on_save = {
-    lsp_format = "fallback",
-    stop_after_first = true,
-    timeout_ms = 500,
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "alejandra" },
+	},
+	format_on_save = {
+		lsp_format = "fallback",
+		stop_after_first = true,
+		timeout_ms = 500,
+	},
 })
-
 
 -- Completion
 require("blink.cmp").setup({
-  sources = {
-    default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-    per_filetype = {
-      lua = { inherit_defaults = true, "lazydev" },
-    },
-    providers = {
-      lazydev = {
-        name = "LazyDev",
-        module = "lazydev.integrations.blink",
-        score_offset = 100,
-      },
-    },
-  },
+	sources = {
+		default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+		per_filetype = {
+			lua = { inherit_defaults = true, "lazydev" },
+		},
+		providers = {
+			lazydev = {
+				name = "LazyDev",
+				module = "lazydev.integrations.blink",
+				score_offset = 100,
+			},
+		},
+	},
 })
 
 -- Icons (used by fzf-lua)
-require('nvim-web-devicons').setup()
+require("nvim-web-devicons").setup()
 
 -- Fuzzy finder
 require("fzf-lua").setup({
-  files = { -- Ignore .git and .jj folders
-    find_opts = [[-type f \! -path '*/.git/*' \! -path '*/.jj/*']],
-    rg_opts   = [[--color=never --hidden --files -g "!.git" -g "!.jj"]],
-    fd_opts   = [[--color=never --hidden --type f --type l --exclude .git --exclude .jj"]],
-    dir_opts  = [[/s/b/a:-d -g "!.git" -g "!.jj"]],
-  },
-  winopts = {
-    width = 0.60,
-    preview = {
-      hidden = true,
-    }
-  }
+	files = { -- Ignore .git and .jj folders
+		find_opts = [[-type f \! -path '*/.git/*' \! -path '*/.jj/*']],
+		rg_opts = [[--color=never --hidden --files -g "!.git" -g "!.jj"]],
+		fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude .jj"]],
+		dir_opts = [[/s/b/a:-d -g "!.git" -g "!.jj"]],
+	},
+	winopts = {
+		width = 0.60,
+		preview = {
+			hidden = true,
+		},
+	},
 })
 vim.keymap.set("n", "ff", ":FzfLua files<CR>", { desc = "Find files" })
 vim.keymap.set("n", "fs", ":FzfLua builtin<CR>", { desc = "Find menu" })
 
 -- Floating terminal
 vim.keymap.set({ "n", "t" }, "<C-/>", function()
-  vim.cmd("FloatermToggle")
+	vim.cmd("FloatermToggle")
 end, { desc = "Toggle floating terminal" })
 
-require('floaterm').setup({
-  terminals = {
-    { name = "Project" },
-  },
+require("floaterm").setup({
+	terminals = {
+		{ name = "Project" },
+	},
 })
 
 vim.g.rustaceanvim = {
-  server = {
-    on_attach = function(client, bufnr)
-      local has_bacon = vim.fn.executable("bacon") == 1
-      local name = "bacon (rust)"
+	server = {
+		on_attach = function(client, bufnr)
+			local has_bacon = vim.fn.executable("bacon") == 1
+			local name = "bacon (rust)"
 
-      if has_bacon then
-        local terminals = require("floaterm.state").terminals
-        local add = false;
+			if has_bacon then
+				local terminals = require("floaterm.state").terminals
+				local add = false
 
-        if terminals ~= nil then
-          add = true;
+				if terminals ~= nil then
+					add = true
 
-          for _, ter in ipairs(terminals) do
-            if ter.name == name then
-              add = false
-            end
-          end
-        end
+					for _, ter in ipairs(terminals) do
+						if ter.name == name then
+							add = false
+						end
+					end
+				end
 
-        local bacon_terminal = { name = name, cmd = "bacon run-long" }
+				local bacon_terminal = { name = name, cmd = "bacon run-long" }
 
-        if add then
-          require("floaterm.api").new_term(bacon_terminal)
-        else
-          require('floaterm').setup({
-            terminals = {
-              { name = "Project" },
-              bacon_terminal
-            },
-          })
-        end
-      end
-    end,
-  },
+				if add then
+					require("floaterm.api").new_term(bacon_terminal)
+				else
+					require("floaterm").setup({
+						terminals = {
+							{ name = "Project" },
+							bacon_terminal,
+						},
+					})
+				end
+			end
+		end,
+	},
 }
 
 -- Explorer
 local oil_details = false
 require("oil").setup({
-  columns = { "icon" },
-  watch_for_changes = false,
-  keymaps = {
-    ["<S-h>"] = "<CMD>Oil<CR>",
-    ["<S-l>"] = { "actions.select", mode = "n" },
-    ["gd"] = {
-      desc = "Toggle file detail view",
-      callback = function()
-        oil_details = not oil_details
-        if oil_details then
-          require("oil").set_columns({ "icon", "permissions", "size", })
-        else
-          require("oil").set_columns({ "icon" })
-        end
-      end,
-    },
-  },
-
+	columns = { "icon" },
+	watch_for_changes = false,
+	keymaps = {
+		["<S-h>"] = "<CMD>Oil<CR>",
+		["<S-l>"] = { "actions.select", mode = "n" },
+		["gd"] = {
+			desc = "Toggle file detail view",
+			callback = function()
+				oil_details = not oil_details
+				if oil_details then
+					require("oil").set_columns({ "icon", "permissions", "size" })
+				else
+					require("oil").set_columns({ "icon" })
+				end
+			end,
+		},
+	},
 })
 
-vim.keymap.set('n', '<leader>e', function()
-  if vim.bo.filetype == 'oil' then
-    require("oil.actions").close.callback()
-  else
-    vim.cmd('Oil')
-  end
+vim.keymap.set("n", "<leader>e", function()
+	if vim.bo.filetype == "oil" then
+		require("oil.actions").close.callback()
+	else
+		vim.cmd("Oil")
+	end
 end, { desc = "Toggle explorer" })
 
 -- Utils
-require('mini.surround').setup()
+require("mini.surround").setup()
 
-require('ccc').setup({
-  highlighter = {
-    auto_enable = true,
-    lsp = true,
-  },
+require("ccc").setup({
+	highlighter = {
+		auto_enable = true,
+		lsp = true,
+	},
 })
 vim.keymap.set("n", "<leader>c", ":CccPick<CR>", { desc = "Color picker" })
 vim.keymap.set("n", "<leader>C", ":CccConvert<CR>", { desc = "Convert color" })
 
 -- Treesitter
 vim.api.nvim_create_autocmd("PackChanged", {
-  group = plugin_cmds,
-  pattern = "nvim-treesitter",
-  callback = function()
-    vim.cmd("TSUpdate")
-  end,
+	group = plugin_cmds,
+	pattern = "nvim-treesitter",
+	callback = function()
+		vim.cmd("TSUpdate")
+	end,
 })
 
 require("nvim-treesitter.configs").setup({
-  auto_install = true,
-  highlight = { enable = true, },
+	auto_install = true,
+	highlight = { enable = true },
 })
 
-vim.o.foldlevel  = 100 -- start with a high fold level
+vim.o.foldlevel = 100 -- start with a high fold level
 vim.o.foldmethod = "expr"
-vim.o.foldexpr   = "nvim_treesitter#foldexpr()"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
