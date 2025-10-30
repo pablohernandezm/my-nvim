f0c7c891e97e117d93983ad620b2dab30b4e7493
Pablo Hernández <phernandezm07@gmail.com>
2025-10-30 15:07:35 -0500

    fix(writing): make writing-mode keymaps work in visual mode
    The keymaps in after/plugin/writing.lua only worked in normal mode before.
    This change makes it possible to easily select text across wrapped lines.

--- after/plugin/writing.lua
+++ after/plugin/writing.lua
@@ -11,10 +11,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.linebreak = true
     vim.opt_local.breakindent = true
 
-    vim.keymap.set("n", "j", "gj", opts)
-    vim.keymap.set("n", "k", "gk", opts)
-    vim.keymap.set("n", "0", "g0", opts)
-    vim.keymap.set("n", "$", "g$", opts)
+    vim.keymap.set({ "n", "v" }, "j", "gj", opts)
+    vim.keymap.set({ "n", "v" }, "k", "gk", opts)
+    vim.keymap.set({ "n", "v" }, "0", "g0", opts)
+    vim.keymap.set({ "n", "v" }, "$", "g$", opts)
 
     vim.notify("Writer mode on", vim.log.levels.INFO)
   end,
