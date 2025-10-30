9f52caf268521d53c3ddf322d99425c9d7da7a53
Pablo Hernández <phernandezm07@gmail.com>
2025-10-30 13:30:42 -0500

    feat(which-key): add which-key.nvim

--- init.lua
+++ init.lua
@@ -59,6 +59,7 @@ vim.pack.add({
   { src = "https://github.com/nvzone/floaterm" },
   { src = "https://github.com/nvzone/volt" },
   { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/folke/which-key.nvim" },
 })
 
 -- LSP (more in ./ftplugin/<filetype>.lua)
